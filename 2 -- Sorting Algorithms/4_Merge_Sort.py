652cca2fd9ac2a16ffd856910d3094a40e1a1c95
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-01-21 12:33:53 +0600

    Add files via upload

---
+++ 2 -- Sorting Algorithms/4_Merge_Sort.py
@@ -0,0 +1,60 @@
+'''
+    Best Sorting algorithms is Merge sort.
+
+    Time Complexity     : O(n log n)
+    Space Complexity    : O(n)
+'''
+
+def Merge_Sort(li):
+    # store li element size
+    size = len(li)
+
+    if size > 1:
+        mid = size // 2
+
+        left_arr = li[:mid]
+        l_size = len(left_arr)
+
+        right_arr = li[mid:]
+        r_size = len(right_arr)
+
+        # Sorting left_arr
+        Merge_Sort(left_arr)
+
+        # Sorting right_arr
+        Merge_Sort(right_arr)
+
+        i = j = k = 0
+
+        while i < l_size and j < r_size:
+
+            if left_arr[i] < right_arr[j]:
+
+                li[k] = left_arr[i]
+                i += 1
+            else:
+                li[k] = right_arr[j]
+                j += 1
+
+            k += 1
+
+
+        while i < l_size:
+            li[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < r_size:
+            li[k] = right_arr[j]
+            j += 1
+            k += 1
+
+    return li
+
+
+# Driver Code
+if __name__ == '__main__':
+    li = [5, 1, 3, 2, 4]
+    res = Merge_Sort(li)
+
+    print(res)
