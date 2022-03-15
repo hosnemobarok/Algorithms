f72553907ff3a3f11c1f01a7c668779dec025424
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2022-03-15 22:34:07 +0600

    Update isPrime.py

--- 6 -- Mathematical Algorithms/2 - Prime Factorization and Divisors/isPrime.py
+++ 6 -- Mathematical Algorithms/2 - Prime Factorization and Divisors/isPrime.py
@@ -1,13 +1,16 @@
-
 def isPrime(num):
-
-    if num < 2: return False
-
+    
+    if num <= 1: return False
     if num == 2: return True
+
     if num > 2 and num % 2 == 0: return False
 
-    for i in range(3, num, 2):
-        if num % 2 == 0:
+    root = int(num ** 0.5)
+
+    for i in range(3, root+1, 2):
+        print(i)
+        
+        if num % i == 0:
             return False
     return True
 
